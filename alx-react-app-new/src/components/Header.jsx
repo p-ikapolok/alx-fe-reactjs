6e83b8e9c8352b8c02ad8d76393ba91908c7c3fa
p-ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2025-07-27 20:15:00 +0300

    Update Header.jsx

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,22 +1,13 @@
-import styled from 'styled-components';
-
-const StyledHeader = styled.header`
-    background-color: #2c3e50;
-    color: white;
-    text-align: center;
-    padding: 20px;
-    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-`;
-
-const Title = styled.h1`
-    margin: 0;
-    font-size: 2rem;
-`;
-
 function Header() {
-    return (
-        <StyledHeader>
-            <Title>My Favorite Cities</Title>
-        </StyledHeader>
-    );
+  return (
+    <header style={{ 
+      backgroundColor: 'navy', 
+      color: 'white', 
+      textAlign: 'center',
+      padding: '20px',
+      marginBottom: '20px'
+    }}>
+      <h1 style={{ margin: 0 }}>My Favorite Cities</h1>
+    </header>
+  );
 }
