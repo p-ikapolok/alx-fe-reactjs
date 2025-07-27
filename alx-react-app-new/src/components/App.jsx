f1afccfe10bd435dae54da3e2bb19702150ef60c
p-ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2025-07-27 23:07:49 +0300

    Update App.jsx

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -1,4 +1,5 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Counter from './components/Counter';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -9,6 +10,9 @@ function App() {
     <>
       <WelcomeMessage />
       <Header />
+       <div className="App">
+      <Counter />
+    </div>
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
