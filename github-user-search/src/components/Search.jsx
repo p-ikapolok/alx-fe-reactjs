42677ad71addee83a3d4d6de3a97acba0ecc6ab4
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2025-08-03 22:01:36 +0300

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -22,7 +22,7 @@ const Search = () => {
       const result = await fetchUserData(form.username);
       setUser(result);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
